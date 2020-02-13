9c3093445cb2bb8c973a3f7f7aeda55359582cbb
Charles Abou Haidar <56395727+thisischarles@users.noreply.github.com>
2020-02-13 13:09:18 -0500

    Update GBMap.h

--- GBMap.h
+++ GBMap.h
@@ -5,7 +5,20 @@
 #ifndef COMP345_GBMAP_H
 #define COMP345_GBMAP_H
 
+#include <iostream>
+#include "Cpp-Graph-Library-master/Graph.cpp"
+#include "Cpp-Graph-Library-master/Graph.h"
+#include "Cpp-Graph-Library-master/Node.cpp"
+#include "Cpp-Graph-Library-master/Node.h"
+#include <vector>
 
+using namespace std;
 
+class BoardGame{
+public:
+     int fromNode, toSouthNode, toNorthNode, toEastNode, toWestNode;
+     static void createBoardGame(int numOfPlayers);
+
+};
 
 #endif //COMP345_GBMAP_H
