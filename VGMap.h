6fc662f72afafa5b430a41b7e559b8fbe95b1dbc
Charles Abou Haidar <56395727+thisischarles@users.noreply.github.com>
2020-02-19 21:12:43 -0500

    Setup VGMap.h, still missing some functions!

--- VGMap.h
+++ VGMap.h
@@ -2,13 +2,18 @@
 // Created by Charles Abou Haidar on 2020-01-22.
 //
 
+
 #ifndef COMP345_VGMAP_H
 #define COMP345_VGMAP_H
 
+#include "Cpp-Graph-Library-master/Graph.h"
 
-class VGMap {
-
+class VGMap{
+public:
+    Graph *vgBoard;
+    VGMap();
+    ~VGMap();
+    void checkDoublePoints(VGMap *vgBoard);
 };
-
 
 #endif //COMP345_VGMAP_H
