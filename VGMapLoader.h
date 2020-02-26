6319554698d3e21e0ad71d47dce84efe0ff89501
andrehalrumhein <60198129+andrehalrumhein@users.noreply.github.com>
2020-02-26 17:57:32 -0500

    Update VGMapLoader.h

--- VGMapLoader.h
+++ VGMapLoader.h
@@ -5,12 +5,20 @@
 #ifndef COMP345_VGMAPLOADER_H
 #define COMP345_VGMAPLOADER_H
 
-
+#include "Cpp-Graph-Library-master/Graph.h"
+#include "Cpp-Graph-Library-master/Node.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 class VGMapLoader {
 
-};
+public:
+    VGMapLoader();
+    void output(string nodeNumber, string data);
 
+};
 
 
 #endif //COMP345_VGMAPLOADER_H
