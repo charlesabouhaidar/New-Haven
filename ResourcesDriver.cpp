c90549aa50c79b72ca0b62b2c5d13b775e5ac29d
alexbriere <43627346+alexbriere@users.noreply.github.com>
2020-02-26 16:56:49 -0500

    Update ResourcesDriver.cpp

--- ResourcesDriver.cpp
+++ ResourcesDriver.cpp
@@ -43,7 +43,7 @@ int main(int argc, const char * argv[]) {
   
 	GBMap board = new GBMap(3);
 
-    hand->exchange(board, 1, 0, 4, 2);
+    	hand->exchange(board, 1, 0, 4, 2);
 	hand->toString();
 
 
