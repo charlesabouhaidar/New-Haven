797e4b9ab7b4b8ec9e8e5fc729346805c568c60b
Charles Abou Haidar <56395727+thisischarles@users.noreply.github.com>
2020-02-26 16:39:19 -0500

    Update GBMapLoader.h

--- GBMapLoader.h
+++ GBMapLoader.h
@@ -1,16 +1,16 @@
 //
 // Created by Charles Abou Haidar on 2020-01-22.
 //
-
+#include <string>
+#include "GBMap.h"
+using namespace std;
 #ifndef COMP345_GBMAPLOADER_H
 #define COMP345_GBMAPLOADER_H
 
-
-
 class GBMapLoader {
-
+public:
+    GBMapLoader();
+    void output(string nodeNumber, string data);
 };
-
-
 
 #endif //COMP345_GBMAPLOADER_H
