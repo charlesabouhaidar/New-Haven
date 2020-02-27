b07cbcab1d73e07728b0a5421616b92b5188ded8
Charles Abou Haidar <56395727+thisischarles@users.noreply.github.com>
2020-02-27 12:22:47 -0500

    Final VGMap

--- VGMap.h
+++ VGMap.h
@@ -2,15 +2,23 @@
 // Created by Charles Abou Haidar on 2020-01-22.
 //
 
+#include <string>
+using std::string;
 #ifndef COMP345_VGMAP_H
 #define COMP345_VGMAP_H
 
+#include "Cpp-Graph-Library-master/Graph.h"
 
-
-class VGMap {
-
+class VGMap{
+public:
+    Graph *vgBoard;
+    VGMap();
+    ~VGMap();
+    Graph* getBoard();
+    string getNorth(string position);
+    string getSouth(string position);
+    string getEast(string position);
+    string getWest(string position);
 };
-
-
 
 #endif //COMP345_VGMAP_H
