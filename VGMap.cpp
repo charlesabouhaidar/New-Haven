e2055cc3434ba1f0988f30ed0c2023882727681f
SwigOf <33262954+SwigOf@users.noreply.github.com>
2020-02-27 14:06:44 -0500

    VGMap returned helper functions

--- VGMap.cpp
+++ VGMap.cpp
@@ -1,54 +1,55 @@
 #include "VGMap.h"
-#include <iostream>
-#include <vector>
+#include "Cpp-Graph-Library-master/Graph.h"
 
 using std::string;
-using std::vector;
 using std::to_string;
 
-VGMap::VGMap(){
-    vgBoard = new Graph(false);
+VGMap::VGMap(string name){
+    resourceFlags = new vector<bool>;
+    resourceFlags->assign(4, false);
+    villageName = name;
+    board = new Graph(false);
     vector<string> reachableNodes;
     int nodeName, fromNode, toNorthNode, toSouthNode, toEastNode, toWestNode, i, j, k, l, m;
     Node *node[30];
     for( i = 0; i < 30; i++){ //create 25 nodes for the vgboard
         node[i] = new Node(0, to_string(i));
-        vgBoard->addNode(0, to_string(i));
+        board->addNode(0, to_string(i));
     }
-    for(j = 0; j < 5; j++){ //first 5 nodes all have south nodes, but no north nodes
+    for(j = 0; j < 5; j++){
         fromNode = j;
         toSouthNode = fromNode + 5;
         toEastNode = fromNode + 1;
         toWestNode = fromNode - 1;
-        vgBoard->addEdge(to_string(fromNode), to_string(toSouthNode));
-        if(fromNode == 0){ //top left node (no west node)
-            vgBoard->addEdge(to_string(fromNode), to_string(toEastNode));
+        board->addEdge(to_string(fromNode), to_string(toSouthNode));
+        if(fromNode == 0){
+            board->addEdge(to_string(fromNode), to_string(toEastNode));
         }
-        else if(fromNode == 4){ //top right node (no east node)
-            vgBoard->addEdge(to_string(fromNode), to_string(toWestNode));
+        else if(fromNode == 4){
+            board->addEdge(to_string(fromNode), to_string(toWestNode));
         }
-        else{ //all other nodes that aren't in corners
-            vgBoard->addEdge(to_string(fromNode), to_string(toEastNode));
-            vgBoard->addEdge(to_string(fromNode), to_string(toWestNode));
+        else{
+            board->addEdge(to_string(fromNode), to_string(toEastNode));
+            board->addEdge(to_string(fromNode), to_string(toWestNode));
         }
     }
-    for(k = 5; k < 25; k++){ //remaining nodes until right before the last row
+    for(k = 5; k < 25; k++){
         fromNode = k;
         toSouthNode = fromNode + 5;
         toNorthNode = fromNode - 5;
         toEastNode = fromNode + 1;
         toWestNode = fromNode - 1;
-        vgBoard->addEdge(to_string(fromNode), to_string(toNorthNode)); //all these nodes have a north node
-        vgBoard->addEdge(to_string(fromNode), to_string(toSouthNode)); //all these nodes have a south node
+        board->addEdge(to_string(fromNode), to_string(toNorthNode));
+        board->addEdge(to_string(fromNode), to_string(toSouthNode));
         if(fromNode % 5 == 0){//left end side of vg board, not connected to west nodes
-            vgBoard->addEdge(to_string(fromNode), to_string(toEastNode));
+            board->addEdge(to_string(fromNode), to_string(toEastNode));
         }
         else if(fromNode % 5 == 4){ //right end side of vg board, not connected to east nodes
-            vgBoard->addEdge(to_string(fromNode), to_string(toWestNode));
+            board->addEdge(to_string(fromNode), to_string(toWestNode));
         }
-        else{ //connected to 4 nodes (all other cases)
-            vgBoard->addEdge(to_string(fromNode), to_string(toWestNode));
-            vgBoard->addEdge(to_string(fromNode), to_string(toEastNode));
+        else{ //connected to 4 nodes
+            board->addEdge(to_string(fromNode), to_string(toWestNode));
+            board->addEdge(to_string(fromNode), to_string(toEastNode));
         }
     }
     for( l = 25; l < 30; l++){
@@ -56,32 +57,46 @@ VGMap::VGMap(){
         toNorthNode = fromNode - 5;
         toEastNode = fromNode + 1;
         toWestNode = fromNode - 1;
-        vgBoard->addEdge(to_string(fromNode), to_string(toNorthNode)); //all these nodes have north nodes but no south nodes (bottom of the board)
-        if(fromNode == 25){ //bottom left corner doesn't have a west node
-            vgBoard->addEdge(to_string(fromNode), to_string(toEastNode));
+        board->addEdge(to_string(fromNode), to_string(toNorthNode));
+        if(fromNode == 25){
+            board->addEdge(to_string(fromNode), to_string(toEastNode));
         }
-        else if(fromNode == 29){ //bottom right corner doesn't have an east node
-            vgBoard->addEdge(to_string(fromNode), to_string(toWestNode));
+        else if(fromNode == 29){
+            board->addEdge(to_string(fromNode), to_string(toWestNode));
         }
-        else{ //all other cases that have both east and west nodes
-            vgBoard->addEdge(to_string(fromNode), to_string(toWestNode));
-            vgBoard->addEdge(to_string(fromNode), to_string(toEastNode));
+        else{
+            board->addEdge(to_string(fromNode), to_string(toWestNode));
+            board->addEdge(to_string(fromNode), to_string(toEastNode));
         }
     }
-
-
 }
+
+VGMap::VGMap() : VGMap("unspecified"){}
 
 VGMap::~VGMap()= default;
 
-
-Graph* VGMap::getBoard() {
-    return vgBoard;
+string VGMap::getName(){
+    return villageName;
 }
 
+Graph* VGMap::getBoard() {
+    return board;
+}
+
+vector<bool>* VGMap::getFlags() {
+    return resourceFlags;
+}
+
+double VGMap::getTileData(string position) {
+    return board->getNodeValue(position);
+}
+
+void VGMap::setTileData(string position, double data) {
+    board->setNodeValue(position, data);
+}
 
 string VGMap::getNorth(string position){
-    vector<string> neighbors = vgBoard->neighborNames(position);
+    vector<string> neighbors = board->neighborNames(position);
     for (int i = 0; i < neighbors.size(); i++) {
         if(stoi(neighbors.at(i)) <= stoi(position)-5)
             return neighbors.at(i);
@@ -89,7 +104,7 @@ string VGMap::getNorth(string position){
     return "";
 }
 string VGMap::getSouth(string position){
-    vector<string> neighbors = vgBoard->neighborNames(position);
+    vector<string> neighbors = board->neighborNames(position);
     for (int i = 0; i < neighbors.size(); i++) {
         if(stoi(neighbors.at(i)) >= stoi(position)+5)
             return neighbors.at(i);
@@ -97,7 +112,7 @@ string VGMap::getSouth(string position){
     return "";
 }
 string VGMap::getEast(string position){
-    vector<string> neighbors = vgBoard->neighborNames(position);
+    vector<string> neighbors = board->neighborNames(position);
     for (int i = 0; i < neighbors.size(); i++) {
         if(stoi(neighbors.at(i)) == stoi(position)+1)
             return neighbors.at(i);
@@ -105,13 +120,12 @@ string VGMap::getEast(string position){
     return "";
 }
 string VGMap::getWest(string position){
-    vector<string> neighbors = vgBoard->neighborNames(position);
+    vector<string> neighbors = board->neighborNames(position);
     for (int i = 0; i < neighbors.size(); i++) {
         if(stoi(neighbors.at(i)) == stoi(position)-1)
             return neighbors.at(i);
     }
     return "";
 }
-
 
 
--- VGMap.h
+++ VGMap.h
@@ -1,17 +1,28 @@
 #ifndef COMP345_VGMAP_H
 #define COMP345_VGMAP_H
 
-#include "Cpp-Graph-Library-master/Graph.h"
 #include <string>
+#include "Cpp-Graph-Library-master/Graph.h"
+#include <vector>
 
 using std::string;
+using std::vector;
 
 class VGMap{
+private:
+    string villageName;
+    vector<bool>* resourceFlags;
 public:
-    Graph *vgBoard;
+    Graph *board;
+    VGMap(string name);
     VGMap();
     ~VGMap();
+
+    string getName();
     Graph* getBoard();
+    vector<bool>* getFlags();
+    double getTileData(string position);
+    void setTileData(string position, double data);
     string getNorth(string position);
     string getSouth(string position);
     string getEast(string position);
