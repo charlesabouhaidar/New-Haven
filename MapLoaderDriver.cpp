b74cc1e85b47c78b355679f848d6b91b60bdd252
Charles Abou Haidar <56395727+thisischarles@users.noreply.github.com>
2020-02-29 02:49:39 -0500

    Final MapLoaderDriver
    Test case are in the GBMapLoader and VGMapLoader, this just chooses which type of map to load

--- MapLoaderDriver.cpp
+++ MapLoaderDriver.cpp
@@ -1,4 +1,25 @@
 //
 // Created by Charles Abou Haidar on 2020-01-22.
 //
+#include <iostream>
+#include "GBMapLoader.h"
+#include "VGMapLoader.h"
+using namespace std;
 
+int main(){
+    int mapToLoad;
+    cout << "Choose an option:\n1. Load GBMap\n2. Load VGMap" << endl;
+    cin >> mapToLoad;
+    switch(mapToLoad){
+        case 1: {
+            GBMapLoader *gbMap = new GBMapLoader();
+            break;
+        }
+        case 2:{
+            VGMapLoader *vgMap = new VGMapLoader();
+        }
+        default:{
+            break;
+        }
+    }
+}
