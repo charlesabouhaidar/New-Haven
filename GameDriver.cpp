bf99bea31e591ed72824290f05a0cd1c08f935c9
SwigOf <33262954+SwigOf@users.noreply.github.com>
2020-03-06 17:08:41 -0500

    Added GameDriver, updated cmake, removed old drivers.

---
+++ GameDriver.cpp
@@ -0,0 +1,14 @@
+#include "GBMap.h"
+#include "Player.h"
+#include "Resources.h"
+#include <iostream>
+#include <string>
+
+
+using std::cout;
+using std::cin;
+using std::string;
+
+int main(){
+
+}
