53b1d73d0ea710ac36800830ca17556c33419b6b
Charles Abou Haidar <56395727+thisischarles@users.noreply.github.com>
2020-04-03 17:39:32 -0400

    Create VGMap object and minor updates

--- VGMapLoader.cpp
+++ VGMapLoader.cpp
@@ -1,6 +1,7 @@
 #include "Cpp-Graph-Library-master/Graph.h"
 #include "Cpp-Graph-Library-master/Node.h"
 #include "VGMapLoader.h"
+#include "VGMap.h"
 #include <iostream>
 
 using std::ifstream;
@@ -12,30 +13,38 @@ using std::endl;
 VGMapLoader::VGMapLoader() {
     ifstream myFile("../VGMapLoad.txt");
     string mystring, data, nodeNumber;
-    Graph *board = new Graph(false);
+   // Graph *board = new Graph(false);
     if(!myFile.is_open()){
         perror("Error opening");
         exit(EXIT_FAILURE);
     }
     else {
-        while (getline(myFile, mystring)) {
-            if(mystring.empty()){ //if empty line just skip
-                continue;
-            }
-            else if(mystring.size() > 7){ //format is: nodeNumber data (total of 7 characters)
-                perror("Incorrect format, map can't load");
-                exit(EXIT_FAILURE);
-            }
-            istringstream var(mystring);
-            var >> nodeNumber >> data; //first number is the node number, second number is the data inside of that node(topRight,topLeft,bottomRight,bottomLeft, playerID)
-            output(nodeNumber, data);
-            board->addNode(stod(data), nodeNumber);
+        if(myFile.peek() == std::ifstream::traits_type.eof()){
+            VGMap *vgMap = new VGMap();
         }
-        myFile.close();
+        else{
+            while (getline(myFile, mystring)) {
+                if(mystring.empty()){ //if empty line just skip
+                    continue;
+                }
+                else if(mystring.size() > 6){ //format is: nodeNumber data (total of 5 characters)
+                    perror("Incorrect format, map can't load");
+                    exit(EXIT_FAILURE);
+                }
+                VGMap *vgMap = new VGMap();
+                istringstream var(mystring);
+                var >> nodeNumber >> data; //first number is the node number, followed by type, numValue, flipped or not
+                vgMap->getBoard()->addNode(stod(data), nodeNumber); //add data to nodeNumber in vgMap
+                //output(nodeNumber, data);
+                // board->addNode(stod(data), nodeNumber);
+            }
+            myFile.close();
+        }
     }
 }
-
+/*
 void VGMapLoader::output(string nodeNumber, string data){
     cout << "\nNode number: " << nodeNumber << endl;
     cout << "Data: " << data << endl;
 }
+*/
