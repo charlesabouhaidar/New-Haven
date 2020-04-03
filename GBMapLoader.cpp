4584aac4b9abd8b1e6e1bc0fc61cdedab5af8293
Charles Abou Haidar <56395727+thisischarles@users.noreply.github.com>
2020-04-03 18:58:23 -0400

    GBMapLoader takes in numberOfPlayers

--- GBMapLoader.cpp
+++ GBMapLoader.cpp
@@ -9,20 +9,17 @@ using std::istringstream;
 using std::cout;
 using std::endl;
 
-GBMapLoader::GBMapLoader() {
+GBMapLoader::GBMapLoader(int numberOfPlayers) {
     ifstream myFile("../GBMapLoad.txt");
     string mystring, data, nodeNumber;
-    int numOfPlayers = 0;
+    GBMap *gbMap = new GBMap(numberOfPlayers);
+    //int numOfPlayers = 0;
    // Graph *board = new Graph(false);
     if (!myFile.is_open()) {
         perror("Error opening");
         exit(EXIT_FAILURE);
     }
     else {
-        if(myFile.peek() == std::ifstream::traits_type::eof()){ //if file is empty, create GBMap object without num of players
-            GBMap *gbMap = new GBMap();
-        }
-        else{ //if file not empty, load data onto GBMap object
             while (getline(myFile, mystring)) {
                 if(mystring.empty()){ //if empty line just skip
                     continue;
@@ -33,17 +30,17 @@ GBMapLoader::GBMapLoader() {
                 }
                 istringstream var(mystring);
                 var >> nodeNumber >> data; //first number is the node number, second number is the data inside of that node(topRight,topLeft,bottomRight,bottomLeft, playerID)
-                char num = data.back(); //get last character of string to find playerID (1-4) !!!ASSUME THAT ALL PLAYERS HAVE PLAYED AT LEAST ONE MOVE!!!
+                /*char num = data.back(); //get last character of string to find playerID (1-4) !!!ASSUME THAT ALL PLAYERS HAVE PLAYED AT LEAST ONE MOVE!!!
                 int number = num - '0';
                 if(numOfPlayers > number ){
                     numOfPlayers = num;
                 }
-                GBMap *gbMap = new GBMap(numOfPlayers);
+                 */
                 //output(nodeNumber, data); check if values are taken in correctly
                 gbMap->getBoard()->addNode(stod(data), nodeNumber);
             }
             myFile.close();
-        }
+        
     }
 }
 
