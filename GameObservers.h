c5d463381d9882f10d2c5b4b9874b67a6cc82a2d
Charles Abou Haidar <56395727+thisischarles@users.noreply.github.com>
2020-04-13 22:01:46 -0400

    Set layout for observers pattern

--- GameObservers.h
+++ GameObservers.h
@@ -1,26 +1,72 @@
 #ifndef GAME_GAMEOBSERVERS_H
 #define GAME_GAMEOBSERVERS_H
-#include <list>
-
-using std::list;
-
-class Observer{
-public:
-    ~Observer();
-    virtual void Update() = 0;
-protected:
-    Observer();
-};
 
 class Subject{
-public:
-    virtual void Attach(Observer* o);
-    virtual void Detach(Observer* o);
-    virtual void Notify();
-    Subject();
-    ~Subject();
 private:
-    list<Observer*> *_observers;
+    std::vector<Observer *> obs;
+  	int *state;
+
+public:
+    void attach(Observer *o);
+    void detach(Observer *o);
+    virtual void notifyAllObservers();
+  	int getState();
+    void setState(int i);
 };
 
+class Observer {
+protected:
+		Subject *subject;  
+public:
+    virtual void update();
+};
+
+class GameStateObserver : public Observer{
+
+  public:
+    GameStateObserver(Subject *subject);
+  	virtual void update() update;
+};
+/*
+ * Place Tile Observer (notifies game state observer when a tile is placed on gb)
+ */
+class PlaceTileObserver : public Observer {
+
+public:
+    PlaceTileObserver(Subject *subject);
+
+    virtual void update() override;
+};
+
+class PlaceBuildingObserver : public Observer {
+
+public:
+    PlaceBuildingObserver(Subject *subject);
+
+    virtual void update() override;
+};
+
+class DrawObserver : public Observer {
+
+public:
+    DrawObserver(Subject *subject);
+
+    virtual void update() override;
+};
+
+class ResetObserver : public Observer {
+
+public:
+    ResetObserver(Subject *subject);
+
+    virtual void update() override;
+};
+
+class ComputeScoreObserver : public Observer{
+public:
+  ComputeScoreObserver(Subject *subject);
+  
+  virtual void update() override;
+}
 #endif //GAME_GAMEOBSERVERS_H
+
